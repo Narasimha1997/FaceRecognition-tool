25edc6f3d6fd2c3506f1797d5e33e938145419a7
Narasimha1997 <yogaimpression@gmail.com>
2018-01-30 21:12:30 +0530

    JJ

--- FaceGenerator.py
+++ FaceGenerator.py
@@ -10,6 +10,8 @@ while True:
     _, frame = camera.read()
     cv2.imwrite("KnownFaces/"+NAME+'.jpg', frame)
     cv2.imshow("Press q to save, quit and generate encoding", frame)
-    if cv2.waitKey(20)
+    if cv2.waitKey(20) & 0xFF = ord('q'):
+        Encodings.encode_single(filename = 'KnownFaces/'+NAME+'.jpg')
+        print('Image captured and finsished generating encoding for it, now run RealTime.py')
 
 
